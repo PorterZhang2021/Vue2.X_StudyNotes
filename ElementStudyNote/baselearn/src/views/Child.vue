ecd02071ffcf7f47e279ad1dd294eb9621dd7dac
PorterZhang <porterzhang@aliyun.com>
2022-02-09 21:00:36 +0800

    [Add] Vue中组件间传值常用的几种方式 props%/%

---
+++ ElementStudyNote/baselearn/src/views/Child.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h3>Child</h3>
+    <h5>{{msg}}</h5>
+    <button @click="passMsg">走你</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Child",
+  props: {
+    msg: {
+      type: String,
+      default: '',
+    }
+  },
+  methods: {
+    passMsg() {
+      this.$emit('showMsg', 'I am from Child');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ ElementStudyNote/baselearn/src/views/Parent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Parent</h1>
+    <child v-bind:msg="'from Parent msg'" @showMsg="showMsg"></child>
+    <h3>{{msg}}</h3>
+  </div>
+</template>
+
+<script>
+import Child from "@/views/Child";
+export default {
+  name: "Parent",
+  data() {
+    return {
+      msg: '',
+    }
+  },
+  components: {
+    Child,
+  },
+  methods: {
+    showMsg(val){
+      this.msg = val;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
