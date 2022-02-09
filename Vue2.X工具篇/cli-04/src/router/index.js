1c0f22be9360b8260779cedec979b84577d3211d
PorterZhang <porterzhang@aliyun.com>
2022-02-09 13:51:45 +0800

    [Add] 课时4 Router入门和安装

---
+++ Vue2.X工具篇/cli-04/src/router/index.js
@@ -0,0 +1,43 @@
+// 引入Vue.js
+import Vue from 'vue'
+// 引入vue-router.js
+import VueRouter from 'vue-router'
+// 引入Home组件
+import Home from '../views/Home.vue'
+import List from "@/views/List";
+
+// 可以参考API的解释，参数是一个插件，路由就是一个插件
+// 这句话的意思是：初始化这个插件以便使用
+Vue.use(VueRouter)
+
+// 这个常量用于设置每个组件的信息，然后交给路由插件注册
+const routes = [
+  {
+    path: '/',  // 链接地址
+    name: 'Home', // 别名
+    component: Home // 加载的组件
+  },
+  {
+    path: '/list',  // 链接地址
+    name: 'List', // 别名
+    component: List // 加载的组件
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // 另一种组件加载模式，路由懒加载，后面会讲
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+// 实例化路由组件
+const router = new VueRouter({
+  // mode : 'history',  // 默认mode : 'hash'
+  routes
+})
+
+// 导出路由
+export default router
---
+++ Vue2.X工具篇/cli-04/src/views/List.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>这里是List页面组件</h3>
+</template>
+
+<script>
+export default {
+  name: "List"
+}
+</script>
+
+<style scoped>
+
+</style>
